bb0f91e270e7fadd8689c1c5beef92750e9f397d
Jimbow <adeathjohn@gmail.com>
2014-03-11 17:23:31 +0800

    bootstrap 更新，並上傳到 wabis 0lx

---
+++ index_files/vlb_engine/vlbdata3.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:true,borderSize:10,classNames:'vlightbox3',descSliding:true,enableRightClick:false,enableSlideshow:true,prefix:'vlb3',resizeSpeed:7,slideTime:4,startZoom:true}) });
